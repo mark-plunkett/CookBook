951175b534dacad4f37d9f3715767ec990b3b88d
mark.plunkett <markje.p@gmail.com>
2021-04-21 14:08:43 +0100

    Implemented list sorting.

--- CookBook.Web.React/src/components/Recipes.js
+++ CookBook.Web.React/src/components/Recipes.js
@@ -6,6 +6,7 @@ import { RecipeListTile } from './RecipeListTile';
 import Icon from 'react-bulma-components/lib/components/icon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import _ from 'lodash';
 
 const { Input, Field, Control } = Form;
 
@@ -14,13 +15,21 @@ export class Recipes extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { recipes: [], loading: true };
+        this.state = {
+            recipes: [],
+            loading: true,
+            orderBy: 'createdOn'
+        };
         this.setState = this.setState.bind(this)
         this.recipeSubscription$ = null;
     }
 
     async componentDidMount() {
-        this.recipeSubscription$ = recipeStore.subscribe(this.setState);
+        this.recipeSubscription$ = recipeStore.subscribe(state => this.setState({
+            ...state,
+            loading: state.loading,
+            recipes: state.recipes
+        }));
         await recipeStore.init();
     }
 
@@ -28,10 +37,18 @@ export class Recipes extends Component {
         this.recipeSubscription$.unsubscribe();
     }
 
-    static renderRecipes(recipes) {
+    onTabSelected(field) {
+        this.setState({
+            ...this.state,
+            orderBy: field
+        });
+    }
+
+    renderRecipes(recipes) {
+        const sorted = _.sortBy(recipes, [this.state.orderBy]);
         return (
-            <Columns>
-                {recipes.map(recipe =>
+            <Columns className="mt-0">
+                {sorted.map(recipe =>
                     <Columns.Column key={recipe.id} size={3}>
                         <RecipeListTile key={recipe.id} recipe={recipe} />
                     </Columns.Column>
@@ -40,54 +57,59 @@ export class Recipes extends Component {
         );
     }
 
+    renderHeader() {
+        return <Container className="pb-3">
+            <Level renderAs="nav">
+                <Level.Side align="left">
+                    <Level.Item>
+                        <Heading size={4}>
+                            Recipes
+                            </Heading>
+                    </Level.Item>
+                </Level.Side>
+                <Level.Side align="right">
+                    <Level.Item>
+                        <Field kind="addons">
+                            <Control>
+                                <Input placeholder="Find a recipe..." value="" readOnly />
+                            </Control>
+                            <Control>
+                                <Button renderAs="button">
+                                    Search
+                                    </Button>
+                            </Control>
+                        </Field>
+                    </Level.Item><Level.Item>
+                        <Button to="/recipes/create" renderAs={Link} className="is-success is-light">
+                            <Icon>
+                                <FontAwesomeIcon icon={faPlus} />
+                            </Icon>
+                            <span>Add Recipe</span>
+                        </Button>
+                    </Level.Item>
+                </Level.Side>
+            </Level>
+        </Container>;
+    }
+
     render() {
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
-            : Recipes.renderRecipes(this.state.recipes);
+            : this.renderRecipes(this.state.recipes);
 
         return (
             <Element>
-                <Container className="pb-3">
-                    <Level renderAs="nav">
-                        <Level.Side align="left">
-                            <Level.Item>
-                                <Heading size={4}>
-                                    Recipes
-                            </Heading>
-                            </Level.Item>
-                        </Level.Side>
-                        <Level.Side align="right">
-                            <Level.Item>
-                                <Field kind="addons">
-                                    <Control>
-                                        <Input placeholder="Find a recipe..." value="" readOnly />
-                                    </Control>
-                                    <Control>
-                                        <Button renderAs="button">
-                                            Search
-                                    </Button>
-                                    </Control>
-                                </Field>
-                            </Level.Item><Level.Item>
-                                <Button to="/recipes/create" renderAs={Link} className="is-success is-light">
-                                    <Icon>
-                                        <FontAwesomeIcon icon={faPlus} />
-                                    </Icon>
-                                    <span>Add Recipe</span>
-                                </Button>
-                            </Level.Item>
-                        </Level.Side>
-                    </Level>
-                </Container>
+                {this.renderHeader()}
                 <Element>
                     <Container>
                         <Tabs className="is-size-7">
-                            <Tabs.Tab active>
+                            <span className="is-italic mr-3">Sort by:</span>
+                            <Tabs.Tab active={this.state.orderBy == 'createdOn'} onClick={() => this.onTabSelected('createdOn')}>
                                 Date Added
-                        </Tabs.Tab>
-                        <Tabs.Tab>
+                            </Tabs.Tab>
+                            <Tabs.Tab active={this.state.orderBy == 'title'} onClick={() => this.onTabSelected('title')}>
                                 Name
-                        </Tabs.Tab>
+                            </Tabs.Tab>
                         </Tabs>
                     </Container>
                     <Element className="has-background-white-bis">
