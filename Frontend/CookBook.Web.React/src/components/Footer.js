516a0540a51e92985d59c4ab9d7d011133b894b4
mark.plunkett <markje.p@gmail.com>
2021-05-01 12:42:14 +0100

    Year now dynamic.

--- Frontend/CookBook.Web.React/src/components/Footer.js
+++ Frontend/CookBook.Web.React/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bulma-components';
 export const Footer = props => {
     return (
         <Container className="my-5">
-            <div className="has-text-right has-text-grey-light"><span>© CookBook 2021</span></div>
+            <div className="has-text-right has-text-grey-light"><span>© CookBook {new Date().getFullYear()}</span></div>
         </Container>
     );
 }
